5d3afdfe998c044d42b0f00414ae1792c34e9125
doylecian <ciandoyle64@gmail.com>
2022-06-14 17:11:46 +0100

    moved contract data to sep file

--- ethereum_wallet_tracker.py
+++ ethereum_wallet_tracker.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from textwrap import wrap
+from eth_contracts import eth_function_hashes, token_contract_decimals
 
 with open('cfg.json') as config:
     cfg = json.load(config)
@@ -14,24 +15,6 @@ api_params = {
     'startblock': startblock,
     'endblock': endblock,
     'apikey': api_key
-}
-
-eth_function_hashes = {
-    '0xe1fffcc4923d04b559f4d29a8bfc6cda04eb5b0d3c460751c2402c5c5cc9109c': 'Deposit',
-    '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef': 'Transfer',
-    '0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822': 'Swap',
-    '0x1c411e9a96e071241c2f21f7726b17ae89e3cab4c78be50e062b03a9fffbbad1': 'Sync',
-    '0x7ff36ab5': 'swapExactETHForTokens',
-    '0x18cbafe5': 'swapExactTokensForETH',
-    '0x38ed1739': 'swapExactTokensForTokens',
-    '0x414bf389': 'exactInputSingle', # todo 0x60c22434deab8cbc3fddcbc1cc85a7f5a21a854f7856454428eeb159ccf9e4a2
-    '0x3805550f': 'exit', # todo 0xcfd8a65b4355b05c36c0d72a04640f16c0f456a630b882a41a78219357999d81
-    '0xa9059cbb': 'transfer(address _to, uint256 _value)' # todo 0x3951c492fd124ca0635b277848200460fcd4effd8a42392e958ca0013a0ed7fb
-}
-
-token_contract_decimals = {
-    'SYN': 18,
-    'DEFAULT': 18
 }
 
 def list_token_txns(wallet_address, token_symbol=0, ascending=False, query_limit=0):
